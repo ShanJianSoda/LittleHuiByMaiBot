2d65718a4572a9407cac98e23f6f6d209c358e3a
ShanJianSoda <1962560763@qq.com>
2026-03-13 14:22:34 +0800

    update

--- src/chat/heartbeat_v2/history_store.py
+++ src/chat/heartbeat_v2/history_store.py
@@ -61,6 +61,7 @@ class HistoryStore:
 
         if limit <= 0:
             return []
+        # TODO：策略获取
         items = list(self._items)[-limit:]
         return [item.to_dict() for item in items]
 
